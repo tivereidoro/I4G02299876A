c40ef754a602bc82c7d35d09d3c53627d36309b4
Tivere IDORO <105525310+tivereidoro@users.noreply.github.com>
2022-07-02 14:25:58 +0100

    well spaced commit

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 
 import random
 
-print("Welcome.!! : Rock-Paper-Scissors CLI game!!")
+print("\n\nWelcome.!! : \nRock-Paper-Scissors CLI game!!\n\n")
 
 start_game = True
 
@@ -9,10 +9,10 @@ while start_game:
     # Take player input
 
     player_input = str(input(
-        "Choose your character: \nEnter 'R' for Rock \nEnter 'P' for Paper \nEnter 'S' for Scissors\nEnter 'Z' to close game.\n"))
+        "Choose your character: \n\nEnter 'R' for Rock \nEnter 'P' for Paper \nEnter 'S' for Scissors\nEnter 'Z' to close game.\n"))
     # checks if it is an alphabet
     if not player_input.isalpha():
-        print("This is not an alphabet")
+        print("This is not an alphabet\n\n")
         continue
 
     # convert player input to uppercase for uniformity
@@ -34,26 +34,26 @@ while start_game:
         print("Both player chose ", player_choice, "It's a tie!")
     elif player_choice == "R":
         if computer_choice == "S":
-            print("Rock smashes scissors! You win!")
+            print("Rock smashes scissors! You win!\n\n")
         else:
-            print("Paper covers rock! You lose.")
+            print("Paper covers rock! You lose.\n\n")
     elif player_choice == "P":
         if computer_choice == "R":
-            print("Paper covers rock! You win!")
+            print("Paper covers rock! You win!\n\n")
         else:
-            print("Scissors cuts paper! You lose.")
+            print("Scissors cuts paper! You lose.\n\n")
 
     elif player_choice == "S":
         if computer_choice == "P":
-            print("Scissors cuts paper! You win!")
+            print("Scissors cuts paper! You win!\n\n")
         else:
-            print("Rock smashes scissors! You lose.")
+            print("Rock smashes scissors! You lose.\n\n")
     else:
         print("Oops, ", player_choice,
-              " is an Invalid input....Let's try that again")
+              " is an Invalid input....Let's try that again\n\n")
 
     restart = input(
-        "Will you like to play again?\nPress 'y' for yes or 'n' for no.")
+        "Will you like to play again?\n\nPress 'y' for yes or 'n' for no.")
     # Restart program
     if restart == "y":
         continue
